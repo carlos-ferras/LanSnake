d9dbf0b462501165f13fd8fc7a7442d31efc7df4
Carlos Feras Hernandez <c4rlos.ferra5@gmail.com>
2015-01-28 00:54:08 -0500

    last

--- wsgi/mdownloader/downloader/views.py
+++ wsgi/mdownloader/downloader/views.py
@@ -50,7 +50,7 @@ def home(request):
         return render_to_response('tform.html',{'message':'ready....'},context_instance=c)
 
 def downloadit(url,mail,start,end):
-    try:
+	#try:
         i = start
         req = urllib2.Request(url, headers={'Range':'bytes='+str(start*m1)+'-'})
         response = urllib2.urlopen(req)
@@ -70,8 +70,8 @@ def downloadit(url,mail,start,end):
             i += 1
             #l += len(buf)
         errmail('term','Size \n'+str(size)+'Sent!!!\n packets from '+str(start)+' to '+str(end)+' were sent with '+url,mail)
-    except:
-        errmail('MDownloader Error','Error Found while downloading '+str(i)+' part of '+url,mail)
+	#~ except:
+        #~ errmail('MDownloader Error','Error Found while downloading '+str(i)+' part of '+url,mail)
 
 
 def deskargar(request):
